896a63cb6d556734ee2b9079b7b810c1a3643dad
mpokutta <>
2021-12-07 17:27:13 -0800

    Arg parse

--- main.py
+++ main.py
@@ -2,38 +2,44 @@
 Main file for training model, visualizing results and preprocessing
 """
 
+import argparse
 from matplotlib.pyplot import fill
 from rnn import run_model
 from visualization_utilities import fill_with_model
 from preprocess_utilities import preprocess
 
-preprocess_bool = False
-train = True
-
-
-raw_data_file = 'data/merged_sst_ice_chl_par_2003.RDS'
-mapping_file = 'data/Bering_full_grid_lookup_no_goa.RDS'
 params_of_interest = ['chlorophyll']
-inputs_path = 'preprocessed-data/2003_50_244_1_1_chlorophyll_inputs.npy'
-labels_path = 'preprocessed-data/2003_50_244_1_1_chlorophyll_labels.npy'
-visualization_path = 'imgs/filled_with_model.gif'
-n = 1
-t = 2
 
 
 
+def parseArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--preprocess_bool", action="store_true")
+    parser.add_argument("--train", action="store_false")
+    parser.add_argument("--raw_data_file", type=str, default='data/merged_sst_ice_chl_par_2003.RDS')
+    parser.add_argument("--mapping_file", type=str, default='data/Bering_full_grid_lookup_no_goa.RDS')
+    parser.add_argument("--inputs_path", type=str, default='preprocessed-data/2003_50_244_1_1_chlorophyll_inputs.npy')
+    parser.add_argument("--labels_path", type=str, default='preprocessed-data/2003_50_244_1_1_chlorophyll_labels.npy')
+    parser.add_argument("--visualization_path", type=str, default='imgs/filled_with_model.gif')
+    parser.add_argument("--t", type=int, default=2)
+    parser.add_argument("--n", type=int, default=1)
+    args = parser.parse_args()
+    return args
 
+def main(args):
+    if args.preprocess_bool:
+            preprocess(args.raw_data_file, args.mapping_file, params_of_interest, 50, 140, t, n, 
+            'preprocessed-data/test_inputs.npy', 'preprocessed-data/test_lables.npy')
+    if args.train:
+        trained_model = run_model(args.inputs_path, args.labels_path, args.n)
+        fill_with_model(trained_model, args.raw_data_file, args.mapping_file, 50, 140, args.t, args.n, args.visualization_path)
 
 
 
 
 
 if __name__ == "__main__":
-    if preprocess_bool:
-        preprocess(raw_data_file, mapping_file, params_of_interest, 50, 140, t, n, 
-            'preprocessed-data/test_inputs.npy', 'preprocessed-data/test_lables.npy')
-    if train:
-        trained_model = run_model(inputs_path, labels_path, n)
-        fill_with_model(trained_model, raw_data_file, mapping_file, 50, 140, t, n, visualization_path)
+    args = parseArguments()
+    main(args)
 
 
